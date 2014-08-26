f8afe31745ff0ef8f8fd8a44eb607e1dd24235a1
David Nguyen <davidnguyen84@gmail.com>
2014-08-26 11:12:40 -0700

    adding and totaling line items

--- app/scripts/controllers/split.js
+++ app/scripts/controllers/split.js
@@ -11,8 +11,26 @@ angular.module('iouApp')
   .controller('SplitCtrl', function ($scope) {
     $scope.splitters = ['David','Tony'];
     $scope.splitter = '';
+    $scope.lineItems = [];
+    $scope.total = 0;
+    $scope.whoPaid = '';
+    $scope.cost = 0;
+    $scope.item = '';
     $scope.addSplitter = function() {
        $scope.splitters.push($scope.splitter);
        $scope.splitter = '';
     };
+    $scope.addLineItem = function() {
+      var lineItem = { 
+        item: $scope.item,
+        cost: $scope.cost,
+        whoPaid: $scope.whoPaid
+      };
+      $scope.lineItems.push(lineItem);
+      $scope.total += lineItem.cost;
+      $scope.item = '';
+      $scope.cost = 0;
+      $scope.whoPaid = '';
+    };
+
   });
