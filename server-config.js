063d1d0ea5a97ad7c4014adc6f932dea996cd9ad
David Nguyen <davidnguyen84@gmail.com>
2014-10-17 16:19:26 -0700

    server/initiated server

--- server-config.js
+++ server-config.js
@@ -1,18 +1,8 @@
 var express = require('express');
-var partials = require('express-partials');
-var util = require('./lib/utility');
-
-var handler = require('./lib/request-handler');
 
 var app = express();
 
-app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.use(express.cookieParser('shhhh, very secret'));
-  app.use(express.session());
-});
+app.use(express.static(__dirname + '/dist'));
 
-app.get('/', util.checkUser, handler.renderIndex);
-app.get('/*', handler.navToLink);
 
 module.exports = app;
