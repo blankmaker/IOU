a749f55e0092cf7ddd664a63d19d0eeef8674297
David Nguyen <davidnguyen84@gmail.com>
2014-10-16 23:09:57 -0700

    refactor/delete about files

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,17 +21,9 @@ angular
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
       .when('/split', {
         templateUrl: 'views/split.html',
         controller: 'SplitCtrl'
-      })
-      .when('/trips', {
-        templateUrl: 'views/trip.html',
-        controller: 'TripCtrl'
       })
       .otherwise({
         redirectTo: '/'
